7b6fb2a821e6e43352e680062da0572d9aa54964
TotallyNotOndre <40639199+TotallyNotOndre@users.noreply.github.com>
2025-06-28 18:45:17 +0300

    fix datafilter
    add ё
    filter extra spaces

--- tools/datafilter/datafilter.js
+++ tools/datafilter/datafilter.js
@@ -5,8 +5,9 @@ const dir = fs.readdir("input", (err, files) => {
 
     files.forEach(file => {
         const data = fs.readFileSync("input/"+file, 'utf-8')
-        .replace(/[^А-я0-9.,"!#?%$():\n ]/g, '')
+        .replace(/[^А-яё0-9.,"!#?%$():\n ]/g, '')
         .replace(/^\d+\s?\n/gm,'')
+        .replace(/ +/gm,' ')
         .replace(/^\n+/gm,'\n');
         fs.writeFile(`output/${file}`, data, (err) => { if (err) console.log(err) })
     })
